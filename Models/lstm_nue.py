2ccaaab62651c838d4e9d8683a163b48a2e0f28b
e <autuanliu@163.com>
2018-09-28 16:05:22 +0800

    add files

---
+++ Models/lstm_nue.py
@@ -0,0 +1,11 @@
+"""
+Email: autuanliu@163.com
+Date: 2018/9/28
+Ref: 
+1. [Estimating Brain Connectivity With Varying-Length Time Lags Using a Recurrent Neural Network](https://ieeexplore.ieee.org/document/8370751/)
+2. [RNN-GC](https://github.com/shaozhefeng/RNN-GC)
+"""
+
+import torch
+import torch.nn.functional as F
+from torch import nn
