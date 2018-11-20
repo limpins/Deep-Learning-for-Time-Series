31b913a983bbc07979c71181312924c63f8e3ac7
AutuanLiu <autuanliu@163.com>
2018-11-20 17:02:22 +0800

    add kalman filter

---
+++ kalman_filter/coef_estimation/kalman_VARX.py
@@ -0,0 +1,7 @@
+"""使用 Kalman Filter 实现对 VARX 模型的系数估计。
+
+Copyright:
+----------
+    Author: AutuanLiu
+    Date: 2018/11/20
+"""
---
+++ kalman_filter/example.py
@@ -0,0 +1,7 @@
+"""测试 Kalman Filter 估计模型系数。
+
+Copyright:
+----------
+    Author: AutuanLiu
+    Date: 2018/11/20
+"""
