3c6d2e1d611b79aa5ed069c0de55e0dd1154f94a
AutuanLiu <autuanliu@163.com>
2018-11-23 12:03:04 +0800

    update

--- kalman_filter/kalman_MVAR.py
+++ kalman_filter/kalman_MVAR.py
@@ -24,20 +24,20 @@ data = get_mat_data(file_path, 'linear_signals')
 data = normalize(data)
 
 # 网格搜索
-lag_range = range(1, 20)
-uc_range = np.arange(0.001, 0.01, 0.0005)
+lag_range = np.arange(3, 6)
+uc_range = np.arange(0.001, 0.01, 0.001)
 # best_lag_uc = grid_search1(data, lag_range, uc_range)
 # print(f'best_lag_uc: {best_lag_uc}')
-best_lag, _ = grid_search2(data, lag_range, 0.001, plot=True)
+best_lag, _ = grid_search2(data, lag_range, 0.001, criterion='AIC', plot=True)
 print(f'best_lag: {best_lag}')
-best_uc, _ = grid_search3(data, 5, uc_range, plot=True)
-print(f'best_uc: {best_uc}')
+# best_uc, _ = grid_search3(data, 5, uc_range, plot=True)
+# print(f'best_uc: {best_uc}')
 
 # 构造 Kalman Filter
 # 初始化
-# max_lag = 19
-# kf = Linear_Kalman_Estimation(data, max_lag, uc=0.0095)
-# y_coef, A_coef = kf.estimate_coef(0.1)
+max_lag = 5
+kf = Linear_Kalman_Estimation(data, max_lag, uc=0.005)
+y_coef, A_coef = kf.estimate_coef(0.1)
 
-# # 保存结果
-# np.savetxt('./kalman_filter/data/y_coef.txt', y_coef, fmt='%.4f', delimiter=', ')
+# 保存结果
+np.savetxt('./kalman_filter/data/y_coef.txt', y_coef, fmt='%.4f', delimiter=', ')
